b96a19f494ea20c971ac18986899f3bc0f117070
Hok Shun Poon <hok@hokshunpoon.me>
2015-10-23 14:00:08 +0100

    Initial Commit

---
+++ HybridTableExperiment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridTableExperiment
+//
+//  Created by Hok Shun Poon on 23/10/2015.
+//  Copyright © 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
